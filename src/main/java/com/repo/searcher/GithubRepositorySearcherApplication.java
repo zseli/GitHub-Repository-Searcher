47da048f2966cdccf59c7949c5d4194514e97da9
seli <selisinha22@gmail.com>
2025-08-19 18:57:39 +0530

    setup: create initial package structure

---
+++ src/main/java/com/repo/searcher/GithubRepositorySearcherApplication.java
@@ -0,0 +1,13 @@
+package com.repo.searcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositorySearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositorySearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repo/searcher/GithubRepositorySearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo.searcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositorySearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
